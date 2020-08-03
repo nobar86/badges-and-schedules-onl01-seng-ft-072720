2b4416b804658550c321926b1e09444e9eaa439b
Michael Rabon <nobar86@gmail.com>
2020-08-03 15:21:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+  
+end
+
+def batch_badge_creator(attendees)
+ ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ 
+end
+
+def assign_rooms(name, room)
+  
+end
